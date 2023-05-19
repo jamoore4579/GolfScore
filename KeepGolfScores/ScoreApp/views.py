3d4de07b78e52672e71f6f814fe7bd90b57f779b
jamoore4579 <jasonamoore@frontier.com>
2023-05-18 22:31:25 -0400

    setup views

--- KeepGolfScores/ScoreApp/views.py
+++ KeepGolfScores/ScoreApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from ScoreApp.models import Courses, Scores
+from ScoreApp.serializers import CourseSerializers, ScoreSerializers
 
 # Create your views here.
+
+@csrf_exempt
+def courseApi(request,id=0):
+    if request.method=='GET':
+        courses = Courses.objects.all()
+        courses_serializer = CourseSerializers(courses, many=True)
+        return JsonResponse(courses_serializer.data, safe=False)
+    elif request.method=='POST':
+        course_data=JSONParser().parse(request)
+        courses_serializer=CourseSerializers(data=course_data)
+        
